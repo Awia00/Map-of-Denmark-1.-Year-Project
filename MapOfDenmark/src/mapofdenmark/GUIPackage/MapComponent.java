54285915f3a6eeade08b1cddd0cd9e2977085898
Anders Wind <awia00@gmail.com>
2014-02-27 12:37:13 +0100

    Added basic GUI classes

---
+++ MapOfDenmark/src/mapofdenmark/GUIPackage/MapComponent.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mapofdenmark.GUIPackage;
+
+
+/**
+ *
+ * @author Anders
+ */
+public class MapComponent {
+    
+}
