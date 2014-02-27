9cf737fbefd633ac17a36776bab19c6ff7efb374
AleksandarJonovic <jonostranz@gmail.com>
2014-02-27 13:44:10 +0100

    Db connection skeleton, subject to major modifications.

---
+++ MapOfDenmark/src/database/Database.java
@@ -0,0 +1,21 @@
+package database;
+
+/**
+ * There can only be one of this class.
+ * Used to connect from classes to the database handler.
+ * @author Andreas Jacobsen
+ */
+public class Database 
+{
+    // The database interface
+    private static DatabaseInterface db = new DatabaseHandler();;
+    
+    /**
+     * This method gets the database interface for use.
+     * @return DatabaseHandler as DatabaseInterface.
+     */
+    public static DatabaseInterface db()
+    {
+        return db;
+    }
+}
---
+++ MapOfDenmark/src/database/DatabaseInterface.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database;
+
+import java.util.ArrayList;
+
+/**
+ * This interface states all public methods available for interaction with the
+ * database, and should provide sufficient coverage for developers wishing to
+ * use it, and it's associated classes to build a user interface.
+ * <p>
+ *
+ * Please note that while SQLExceptions are handled, NullPointerExceptions are
+ * not, unless explicitly stated.
+ *
+ * @author Aleksandar Jonovic
+ */
+public interface DatabaseInterface {
+
+}
