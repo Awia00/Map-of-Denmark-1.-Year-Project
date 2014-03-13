7e264efd72fbe255e0696f1b664776fdb6a54386
Anders Wind <awia00@gmail.com>
2014-03-13 13:21:42 +0100

    moresubtle zoom in and out

--- MapOfDenmark/src/mapofdenmark/GUIPackage/MapComponent.java
+++ MapOfDenmark/src/mapofdenmark/GUIPackage/MapComponent.java
@@ -31,8 +31,8 @@ public class MapComponent extends JComponent{
 	private QuadTree quadTreeToDraw;
 	private VisibleArea visibleArea;
 	
-	private final double zoomInConstant = 0.9;
-	private final double zoomOutConstant = 1.1;
+	private final double zoomInConstant = 0.009;
+	private final double zoomOutConstant = 1.001;
 
 	public MapComponent(VisibleArea visibleArea, Street[] streets)
 	{
