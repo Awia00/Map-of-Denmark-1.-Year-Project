8acffa3fb403c841112c387a5b936adda30c11bf
Anders Wind <awia00@gmail.com>
2014-03-13 13:37:50 +0100

    Zoom in works

--- MapOfDenmark/src/mapofdenmark/GUIPackage/MapComponent.java
+++ MapOfDenmark/src/mapofdenmark/GUIPackage/MapComponent.java
@@ -31,8 +31,8 @@ public class MapComponent extends JComponent{
 	private QuadTree quadTreeToDraw;
 	private VisibleArea visibleArea;
 	
-	private final double zoomInConstant = 0.9999999;
-	private final double zoomOutConstant = 1.0000001;
+	private final double zoomInConstant = 0.9;
+	private final double zoomOutConstant = 2;
 
 	public MapComponent(VisibleArea visibleArea, Street[] streets)
 	{
