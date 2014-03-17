734acb1bd3d02d990d1d167f7ca6347a36da3ccf
ChristianGroen <christiangroen@gmail.com>
2014-03-17 13:50:24 +0100

    added some doc

--- MapOfDenmark/src/mapofdenmark/GUIPackage/ColorScheme.java
+++ MapOfDenmark/src/mapofdenmark/GUIPackage/ColorScheme.java
@@ -14,15 +14,27 @@ package mapofdenmark.GUIPackage;
 import java.awt.Color;
 
 public class ColorScheme {
+    // The name of the color scheme.
     private final String name;
+    // The color of the background. 
     private final Color backgroundColor;
+    // The color of the place name.
     private final Color placeNameColor;
+    // The color of the highway.
     private final Color highwayColor;
+    // The color of the ferryway.
     private final Color ferrywayColor;
+    // The color of the secondary road.
     private final Color secondaryRoadColor;
+    // The color of the pathway.
     private final Color pathwayColor;
+    // The color of the road.
     private final Color roadColor;
     
+    //Constructor for the ColorScheme class. This constructor takes Color 
+    //objects as parameters and initializes the color for the background,
+    //placeName, highway, ferryway, secondaryRoad, pathway and road as well as
+    //setting the name of the ColorScheme. 
     public ColorScheme(String name, Color backgroundColor, Color placeNameColor, Color highwayColor, Color ferrywayColor, Color secondaryRoadColor, Color pathwayColor, Color roadColor)
     {
     this.name = name;
