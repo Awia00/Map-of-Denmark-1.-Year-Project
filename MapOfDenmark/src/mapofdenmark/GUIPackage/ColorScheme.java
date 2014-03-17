b15b0d8d589fe6952044c8cf72faaa2786a26f6a
ChristianGroen <christiangroen@gmail.com>
2014-03-17 15:14:24 +0100

    Updated ColorScheme class

--- MapOfDenmark/src/mapofdenmark/GUIPackage/ColorScheme.java
+++ MapOfDenmark/src/mapofdenmark/GUIPackage/ColorScheme.java
@@ -19,32 +19,35 @@ public class ColorScheme {
     // The color of the background. 
     private final Color backgroundColor;
     // The color of the place name.
-    private final Color placeNameColor;
-    // The color of the highway.
     private final Color highwayColor;
-    // The color of the ferryway.
-    private final Color ferrywayColor;
-    // The color of the secondary road.
+    // The color of the highway.
     private final Color secondaryRoadColor;
+    // The color of the ferryway.
+    private final Color normalRoadColor;
+    // The color of the secondary road.
+    private final Color smallRoadColor;
     // The color of the pathway.
     private final Color pathwayColor;
     // The color of the road.
-    private final Color roadColor;
+    private final Color ferrywayColor;
+    // The color of the place name.'
+    private final Color placeNameColor;
     
     //Constructor for the ColorScheme class. This constructor takes Color 
     //objects as parameters and initializes the color for the background,
     //placeName, highway, ferryway, secondaryRoad, pathway and road as well as
     //setting the name of the ColorScheme. 
-    public ColorScheme(String name, Color backgroundColor, Color placeNameColor, Color highwayColor, Color ferrywayColor, Color secondaryRoadColor, Color pathwayColor, Color roadColor)
+    public ColorScheme(String name, Color backgroundColor, Color highwayColor, Color secondaryRoadColor, Color normalRoadColor, Color smallRoadColor, Color pathwayColor, Color ferrywayColor, Color placeNameColor)
     {
     this.name = name;
     this.backgroundColor = backgroundColor;
-    this.placeNameColor = placeNameColor;
     this.highwayColor = highwayColor;
-    this.ferrywayColor = ferrywayColor;
     this.secondaryRoadColor = secondaryRoadColor;
+    this.normalRoadColor = normalRoadColor;
+    this.smallRoadColor = smallRoadColor;
     this.pathwayColor = pathwayColor;
-    this.roadColor = roadColor;
+    this.ferrywayColor = ferrywayColor;
+    this.placeNameColor = placeNameColor;
     }
 
     public String getName() {
@@ -55,28 +58,32 @@ public class ColorScheme {
         return backgroundColor;
     }
 
-    public Color getPlaceNameColor() {
-        return placeNameColor;
-    }
-
     public Color getHighwayColor() {
         return highwayColor;
-    }
-
-    public Color getFerrywayColor() {
-        return ferrywayColor;
     }
 
     public Color getSecondaryRoadColor() {
         return secondaryRoadColor;
     }
 
+    public Color getNormalRoadColor() {
+        return normalRoadColor;
+    }
+
+    public Color getSmallRoadColor() {
+        return smallRoadColor;
+    }
+
     public Color getPathwayColor() {
         return pathwayColor;
     }
 
-    public Color getRoadColor() {
-        return roadColor;
+    public Color getFerrywayColor() {
+        return ferrywayColor;
     }
-    
+
+    public Color getPlaceNameColor() {
+        return placeNameColor;
+    }
+
 }
