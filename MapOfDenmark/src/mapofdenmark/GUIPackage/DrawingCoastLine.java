8fb48221a2bdb03f3a3e7ba3c503cc4f07b09593
Anders Wind Steffensen <awia00@gmail.com>
2014-03-30 21:03:08 +0200

    class to record data taken from the program

---
+++ MapOfDenmark/src/mapofdenmark/GUIPackage/DrawingCoastLine.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mapofdenmark.GUIPackage;
+
+/**
+ *
+ * @author Anders
+ */
+public class DrawingCoastLine {
+	
+	public static void giveData(double coordX, double CoordY, boolean isLeftClick)
+	{
+		
+	}
+}
