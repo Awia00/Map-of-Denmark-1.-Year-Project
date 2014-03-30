ff4b04da9b66f87f5ea81e8b08e5260660d8de67
Anders Wind Steffensen <awia00@gmail.com>
2014-03-30 21:04:12 +0200

    added functionality to send data to DrawingCoastLine from the mainFrame

--- MapOfDenmark/src/mapofdenmark/GUIPackage/MainFrame.java
+++ MapOfDenmark/src/mapofdenmark/GUIPackage/MainFrame.java
@@ -266,6 +266,19 @@ public class MainFrame extends JFrame implements MouseListener, MouseMotionListe
 	@Override
 	public void mouseClicked(MouseEvent e)
 	{
+		if (pressedKeyCode == 17)
+		{
+			double xCoord = drawMapComponent.convertMouseXToMap(e.getX());
+			double yCoord = drawMapComponent.convertMouseXToMap(e.getY());
+			if (e.getButton() == MouseEvent.BUTTON1)
+			{
+				DrawingCoastLine.giveData(xCoord, yCoord, true);
+			}
+			else
+			{
+				DrawingCoastLine.giveData(xCoord, yCoord, false);
+			}	
+		}
 		if (e.getClickCount()>= 2)
 		{
 			if(e.getButton() == MouseEvent.BUTTON3)
