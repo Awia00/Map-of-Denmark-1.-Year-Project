7b97c039358dcd2cd57368c2f83fbd19fe19f7d1
Anders Wind <awia00@gmail.com>
2014-04-14 15:00:43 +0200

    osm parser class added

---
+++ MapOfDenmark/src/database/osmParser.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package database;
+
+import java.util.ArrayList;
+import mapofdenmark.GUIPackage.QuadTree;
+
+/**
+ * Class description:
+ *
+ * @version 0.1 - changed 14-04-2014
+ * @authorNewVersion  Anders Wind - awis@itu.dk
+ *
+ * @buildDate 14-04-2014
+ * @author Anders Wind - awis@itu.dk
+ */
+public class osmParser implements DatabaseInterface {
+
+	@Override
+	public ArrayList<Edge> getData()
+	{
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	@Override
+	public ArrayList<Node> getListOfNodes()
+	{
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	@Override
+	public double getNodesDownloadedPct()
+	{
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	@Override
+	public double getEdgesDownloadedPct()
+	{
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	@Override
+	public double getStreetsDownloadedPct()
+	{
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	@Override
+	public QuadTree getQuadTree()
+	{
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+}
