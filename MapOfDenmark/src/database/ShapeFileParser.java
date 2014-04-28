75fcfe78b383592dce4b163113f194677746e968
Anders Wind <awia00@gmail.com>
2014-04-28 13:21:27 +0200

    Added a shapeFileParser og ændret memory

---
+++ MapOfDenmark/src/database/ShapeFileParser.java
@@ -0,0 +1,108 @@
+package database;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.nocrala.tools.gis.data.esri.shapefile.ShapeFileReader;
+import org.nocrala.tools.gis.data.esri.shapefile.exception.InvalidShapeFileException;
+import org.nocrala.tools.gis.data.esri.shapefile.header.ShapeFileHeader;
+import org.nocrala.tools.gis.data.esri.shapefile.shape.AbstractShape;
+import org.nocrala.tools.gis.data.esri.shapefile.shape.PointData;
+import org.nocrala.tools.gis.data.esri.shapefile.shape.shapes.MultiPointZShape;
+import org.nocrala.tools.gis.data.esri.shapefile.shape.shapes.PointShape;
+import org.nocrala.tools.gis.data.esri.shapefile.shape.shapes.PolygonShape;
+
+/**
+ * Class description:
+ *
+ * @version 0.1 - changed 28-04-2014
+ * @authorNewVersion Anders Wind - awis@itu.dk
+ *
+ * @buildDate 28-04-2014
+ * @author Anders Wind - awis@itu.dk
+ */
+public class ShapeFileParser {
+
+	ArrayList<PolygonShape> polygons;
+
+	public ShapeFileParser()
+	{
+		polygons = new ArrayList<>();
+		startParsing();
+	}
+
+	public ArrayList<PolygonShape> getPolygons()
+	{
+		return polygons;
+	}
+
+	private void startParsing() 
+	{
+		try{
+			parseLanduse();
+		}catch(	InvalidShapeFileException | IOException ex)
+		{
+			Logger.getLogger(ShapeFileParser.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+
+	private void parseLanduse() throws FileNotFoundException, InvalidShapeFileException, IOException
+	{
+		try (FileInputStream is = new FileInputStream(
+				"assets/shape/landuse.shp"))
+		{
+			ShapeFileReader r = new ShapeFileReader(is);
+
+			ShapeFileHeader h = r.getHeader();
+			System.out.println("The shape type of this files is " + h.getShapeType());
+
+			int total = 0;
+			AbstractShape s;
+			while ((s = r.next()) != null)
+			{
+
+				switch (s.getShapeType())
+				{
+					case POINT:
+						PointShape aPoint = (PointShape) s;
+						// Do something with the point shape...
+						break;
+					case MULTIPOINT_Z:
+						MultiPointZShape aMultiPointZ = (MultiPointZShape) s;
+						// Do something with the MultiPointZ shape...
+						break;
+					case POLYGON:
+						PolygonShape aPolygon = (PolygonShape) s;
+						polygons.add(aPolygon);
+						
+						/* debug code
+						System.out.println("I read a Polygon with "
+								+ aPolygon.getNumberOfParts() + " parts and "
+								+ aPolygon.getNumberOfPoints() + " points");
+						for (int i = 0; i < aPolygon.getNumberOfParts(); i++)
+						{
+							PointData[] points = aPolygon.getPointsOfPart(i);
+							System.out.println("- part " + i + " has " + points.length
+									+ " points.");
+						}
+						*/
+						break;
+					default:
+						System.out.println("Read other type of shape.");
+				}
+				total++;
+			}
+
+			System.out.println("Total shapes read: " + total);
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		new ShapeFileParser();
+	}
+}
