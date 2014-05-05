368d2d19f28b9b1430cc9d37777fec8fd8f5f9e4
Anders Wind Steffensen <awia00@gmail.com>
2014-05-05 23:40:15 +0200

    edges fra osm får nu nodeID 1 i stedet for 0

--- MapOfDenmark/src/database/Edge.java
+++ MapOfDenmark/src/database/Edge.java
@@ -43,15 +43,16 @@ public class Edge implements Comparable<Edge> {
         this.roadType = roadType;
         this.roadName = roadName;
         this.roadcode = roadcode;
-        this.fromNodeID = 0;
-        this.toNodeID = 0;
+        this.fromNodeID = 1;
+        this.toNodeID = 1;
         setMidNode();
         setLength();
         this.weightTime = this.length / velocity;
     }
     
    public void setLength(){
-       if(length == Double.MAX_VALUE) length = Math.sqrt( Math.pow(fromNode.getxCoord()-toNode.getxCoord(),2) + Math.pow(fromNode.getyCoord()-toNode.getyCoord(),2) );
+       if(length == Double.MAX_VALUE) 
+		   length = Math.sqrt( Math.pow(fromNode.getxCoord()-toNode.getxCoord(),2) + Math.pow(fromNode.getyCoord()-toNode.getyCoord(),2) );
    }
     
    public double getLength(){
