933282475e409322234179b922c13778034a4b35
Anders Wind <awia00@gmail.com>
2014-05-20 23:31:00 +0200

    More refactoring

--- MapOfDenmark/test/DataStructureTest/QuadTreeTest.java
+++ MapOfDenmark/test/DataStructureTest/QuadTreeTest.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package mapofdenmark.GUIPackage;
+package DataStructureTest;
 
 import DataStructure.QuadTree;
 import DataStructure.Edge;
